fdcde95faed152b3ccf612052ee34f4c25adf128
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-01-18 19:21:44 +0330

    signin added

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,33 +1,24 @@
 'use client'
 
-import Button from '@/ui/Button'
 import RHFTextField from '@/ui/RHFTextField'
 import React from 'react'
-import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import Button from '@/ui/Button'
+import Link from 'next/link'
+import { signinApi } from 'services/authService'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link'
-import { signupApi } from 'services/authService'
-
-// export const metadata = {
-//   title: 'ثبت نام',
-// }
 
 const schema = yup
   .object({
-    name: yup
-      .string()
-      .min(5, 'نام و نام خانوادگی نامعتبر است')
-      .max(30)
-      .required('نام و نام خانوادگی ضروری است'),
     email: yup.string().email('ایمیل نامعتبر است').required('ایمیل الزامی است'),
     password: yup.string().required('رمز عبور الزامی است'),
   })
   .required()
 
-function Signup() {
+function Signin() {
   const {
     register,
     handleSubmit,
@@ -41,9 +32,9 @@ function Signup() {
 
   const onSubmit = async (values) => {
     try {
-      const { user, message } = await signupApi(values)
+      const { user, message } = await signinApi(values)
+      //   router.push('/profile')
       toast.success(message)
-      router.push('/profile')
     } catch (error) {
       toast.error(error?.response?.data?.message)
     }
@@ -51,43 +42,33 @@ function Signup() {
   return (
     <div>
       <h1 className="text-xl font-bold text-secondary-500 text-center mb-6">
-        ثبت نام
+        ورود
       </h1>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
-        <RHFTextField
-          label="نام و نام خانوادگی"
-          name="name"
-          register={register}
-          isRequired
-          errors={errors}
-        />
         <RHFTextField
           label="ایمیل"
           name="email"
           register={register}
-          isRequired
-          dir="ltr"
           errors={errors}
+          isRequired
         />
         <RHFTextField
           label="رمز عبور"
           name="password"
           register={register}
+          errors={errors}
           isRequired
           type="password"
-          dir="ltr"
-          errors={errors}
         />
         <Button type="submit" variant="primary" className="w-full">
-          ثبت نام
-        </Button>
-
-        <Link href="/signin" className="text-secondary-500 mt-6 text-center">
           ورود
+        </Button>
+        <Link href="/signup" className="text-secondary-500 mt-6 text-center">
+          ثبت نام
         </Link>
       </form>
     </div>
   )
 }
 
-export default Signup
+export default Signin
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,9 @@
 import http from './httpService'
 
-export default async function signupApi(data) {
+export async function signupApi(data) {
   return http.post('/user/signup', data).then(({ data }) => data.data)
+}
+
+export async function signinApi(data) {
+  return http.post('/user/signin', data).then(({ data }) => data.data)
 }
