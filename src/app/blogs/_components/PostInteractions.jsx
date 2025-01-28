bfecec8cf8b8f2446f2c1d5c36cb9f768b5c8b2e
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-01-28 20:21:52 +0330

    bookmark post added

--- src/app/blogs/_components/PostInteractions.jsx
+++ src/app/blogs/_components/PostInteractions.jsx
@@ -4,10 +4,10 @@ import ButtonIcon from '@/ui/ButtonIcon'
 import React from 'react'
 import { IoChatboxEllipsesOutline } from 'react-icons/io5'
 import { FaRegHeart, FaHeart } from 'react-icons/fa'
-import { IoBookmarkOutline } from 'react-icons/io5'
+import { IoBookmarkOutline, IoBookmark } from 'react-icons/io5'
 import { toPersianDigits } from '@/utils/numberFormatter'
 import toast from 'react-hot-toast'
-import { likePostApi } from '@/services/postServices'
+import { bookmarkPostApi, likePostApi } from '@/services/postServices'
 import { useRouter } from 'next/navigation'
 
 function PostInteractions({ post }) {
@@ -24,6 +24,15 @@ function PostInteractions({ post }) {
     }
   }
 
+  const bookmarkHandler = async (postId) => {
+    try {
+      const { message } = await bookmarkPostApi(postId)
+      toast.success(message)
+      router.refresh()
+    } catch (error) {
+      toast.error(error?.response?.data?.message)
+    }
+  }
   return (
     <div className="flex items-center justify-between gap-x-4">
       <ButtonIcon variant="secondary">
@@ -33,8 +42,8 @@ function PostInteractions({ post }) {
       <ButtonIcon variant="red" onClick={() => likeHandler(post._id)}>
         {post.isLiked ? <FaHeart /> : <FaRegHeart />}
       </ButtonIcon>
-      <ButtonIcon variant="primary">
-        <IoBookmarkOutline />
+      <ButtonIcon variant="primary" onClick={() => bookmarkHandler(post._id)}>
+        {post.isBookmarked ? <IoBookmark /> : <IoBookmarkOutline />}
       </ButtonIcon>
     </div>
   )
