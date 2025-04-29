9ea17a9a1337393785a1f7b1b3feb325cefeafd2
Maryam Mohebbi <maryam.mohebbi.jr@gmail.com>
2025-04-29 19:55:54 +0330

    dashboard cards and dashboard data fetching added

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,7 +1,19 @@
+import { fetchCardData } from '@/services/data'
 import React from 'react'
+import Card from './_components/Card'
 
-function Profile() {
-  return <div>Profile</div>
+async function Profile() {
+  const { numberOfUsers, numberOfComments, numberOfPosts } =
+    await fetchCardData()
+  return (
+    <div>
+      <div className="grid gap-6 md:grid-cols-3 mb-8">
+        <Card title="کاربران" value={numberOfUsers} type="users" />
+        <Card title="نظرات" value={numberOfComments} type="comments" />
+        <Card title="پست ها" value={numberOfPosts} type="posts" />
+      </div>
+    </div>
+  )
 }
 
 export default Profile
---
+++ src/services/data.js
@@ -0,0 +1,33 @@
+import setCookieOnReq from '@/utils/setCookieOnReq'
+import { getAllUsersApi } from './authService'
+import { getAllCommentsApi } from './commentService'
+import { getPosts } from './postServices'
+import { cookies } from 'next/headers'
+
+export async function fetchCardData() {
+  const cookieStore = cookies()
+  const options = setCookieOnReq(cookieStore)
+
+  try {
+    const data = await Promise.all([
+      getAllUsersApi(options),
+      getAllCommentsApi(options),
+      getPosts(),
+    ])
+
+    // console.log(data[0].users, data[1], data[2].length)
+
+    const numberOfUsers = Number(data[0].users.length ?? '0')
+    const numberOfComments = Number(data[1].commentsCount ?? '0')
+    const numberOfPosts = Number(data[2].length ?? '0')
+
+    return {
+      numberOfUsers,
+      numberOfComments,
+      numberOfPosts,
+    }
+  } catch (error) {
+    console.log(error.response.data.message)
+    throw new Error('خطا در بارگذاری اطلاعات')
+  }
+}
