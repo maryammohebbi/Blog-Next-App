0685b0e358ee90533058d8618784d05ceb67242e
Maryam Mohebbi <maryam.mohebbi.jr@gmail.com>
2025-04-30 20:19:33 +0330

    search posts in dashboard added

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -2,18 +2,19 @@ import React, { Suspense } from 'react'
 import PostsTable from './posts/_/components/PostsTable'
 import CardsWrapper from './_components/CardsWrapper'
 import Fallback from '@/ui/Fallback'
+import LatestPosts from './_components/LatestPosts'
 
 async function Profile() {
   return (
     <div>
-      <h1>داشبورد</h1>
+      <h1 className="text-xl mb-8 text-secondary-700">داشبورد</h1>
       <Suspense fallback={<Fallback />}>
         <CardsWrapper />
       </Suspense>
 
-      <h2>آخرین پست ها</h2>
+      <h2 className="text-xl mb-8 text-secondary-700">آخرین پست ها</h2>
       <Suspense fallback={<Fallback />}>
-        <PostsTable query="sort=latest&limit=5" />
+        <LatestPosts />
       </Suspense>
     </div>
   )
--- src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
+++ src/app/(dashboard)/profile/posts/_/components/Buttons.jsx
@@ -5,6 +5,19 @@ import Link from 'next/link'
 import React from 'react'
 import { CiTrash } from 'react-icons/ci'
 import { PiPencilLineLight } from 'react-icons/pi'
+import { LuPlus } from 'react-icons/lu'
+
+export function CreatePost() {
+  return (
+    <Link
+      href="/profile/posts/create"
+      className="justify-self-end flex gap-x-4 py-3 items-center rounded-lg bg-primary-900 px-4 text-sm font-medium transition-colors hover:bg-primary-700"
+    >
+      <span className="hidden md:block">ایجاد پست</span>
+      <LuPlus className="w-5 h-5" />
+    </Link>
+  )
+}
 
 export function DeletePost({ id }) {
   return (
